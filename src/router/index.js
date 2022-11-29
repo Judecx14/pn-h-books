0054ec94294f5b6cdcbeb3c892541c576d649a1b
RosaMedina28 <rosamedina1008@gmail.com>
2022-11-29 12:43:42 -0600

    [ADD] component login

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Books from '../components/Admin/Libros.vue'
 import Category from '../components/Admin/Categoria.vue'
 import Cita from '../components/Admin/Cita.vue'
+import Login from '../components/Admin/Login.vue'
 import App from '../App.vue'
 const router = createRouter({
     history: createWebHistory(),
@@ -12,7 +13,8 @@ const router = createRouter({
             {path:'admin',children:[
                 {path:'book', component:Books},
                 {path:'category', component:Category},
-                {path:'cita',component:Cita}
+                {path:'cita',component:Cita},
+                {path:'login', component:Login}
             ]}
         ]}
     ]
