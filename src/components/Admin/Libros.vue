dbefabfb61b1a5cd8c60a7be91ae37b5a7b549a7
AngelZapata582 <angelzapata582@gmail.com>
2022-11-29 20:21:08 -0600

    [UPDATE] book admin functionality;[ADD] enviroment url variable;

--- src/components/Admin/Libros.vue
+++ src/components/Admin/Libros.vue
@@ -1,8 +1,13 @@
 <template>
-    <div class="container">
-        <button type="button" id="myModal" class="btn text-light my-2" style="background-color: #2B5CBA;" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    <div class="container">    
+    <button type="button" id="myModal" class="btn text-light my-2" style="background-color: #2B5CBA;" data-bs-toggle="modal" data-bs-target="#exampleModal">
     Agregar
     </button>
+    <div v-if="loading" class="d-flex justify-content-center">
+            <div class="spinner-border text-primary m-4" role="status">
+                <span class="visually-hidden text-light">Loading...</span>
+            </div>
+        </div>
     <div class="container-fluid text-center">
         <table class="table">
         <thead>
@@ -11,18 +16,22 @@
                 <th scope="col">Titulo</th>
                 <th scope="col">Categoria</th>
                 <th scope="col">Autor</th>
+                <th scope="col">Editorial</th>
+                <th scope="col">ISBN</th>
                 <th scope="col">Portada</th>
                 <th scope="col">PDF</th>
                 <th></th>
             </tr>
         </thead>
         <tbody>
-            <tr class="text-light" v-for="(book,index) in books" :key="book.id">
+            <tr v-if="books.length > 0 && !loading" class="text-light" v-for="(book,index) in books" :key="book.id">
                 <td>{{index + 1}}</td>
-                <td>{{book.title}}</td>
-                <td>{{book.category}}</td>
-                <td>{{book.author}}</td>
-                <td>{{book.portada}}</td>
+                <td>{{book.titulo}}</td>
+                <td>{{book.categoria}}</td>
+                <td>{{book.autor}}</td>
+                <td>{{book.editorial}}</td>
+                <td>{{book.isbn}}</td>
+                <td>{{book.imagen}}</td>
                 <td>{{book.pdf}}</td>
                 <td>
                     <div>
@@ -31,6 +40,9 @@
                     </div>
                 </td>
             </tr>
+            <tr v-if="books.length <= 0 && !loading" class="text-light">
+                <td>No hay Libros</td>
+            </tr>
         </tbody>
     </table>
     </div>
@@ -38,44 +50,63 @@
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog border">
     <div class="modal-content">
       <div class="modal-body" style="background-color: #020202;">
-        <form>
+        <form accept-charset="UTF-8" enctype="multipart/form-data" @submit="save">
             <div class="mb-3 text-light">
                 <label for="titulo" class="form-label">Titulo</label>
-                <input type="text" id="titulo" class="form-control">    
+                <input type="text" id="titulo" class="form-control" v-model="book.titulo">    
             </div>
             <div class="mb-3 text-light">
                 <label class="form-label">Autor</label>
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" v-model="book.autor">
+            </div>
+            <div class="mb-3 text-light">
+                <label class="form-label">Editorial</label>
+                <input type="text" class="form-control" v-model="book.editorial">
+            </div>
+            <div class="mb-3 text-light">
+                <label class="form-label">ISBN</label>
+                <input type="text" class="form-control" v-model="book.isbn">
             </div>
             <div class="mb-3 text-light">
                 <label class="form-label">Categoria</label>
-                <select class="form-select" v-model="category">
-                    <option v-for="option in categories" :value="option.value" :key="option">
-                        {{ option.category }}
+                <select class="form-select" v-model="book.category_id">
+                    <option v-for="option in categories" :value="option.id" :key="option">
+                        {{ option.categoria }}
                     </option>
                 </select>        
             </div>
             <div class="mb-3 text-light">
                 <label class="form-label">Paginas</label>
-                <input type="text" class="form-control">
+                <input type="number" class="form-control" v-model="book.numero_paginas">
+            </div>
+            <div class="mb-3 text-light">
+                <label class="form-label">Edicion</label>
+                <input type="number" class="form-control" v-model="book.edicion">
+            </div>
+            <div class="mb-3 text-light">
+                <label class="form-label">Año de lanzamiento</label>
+                <input type="number" class="form-control" v-model="book.año_lanzamiento">
+            </div>
+            <div class="mb-3 text-light">
+                <label class="form-label">sinopsis</label>
+                <textarea type="textarea" class="form-control" v-model="book.sinopsis"></textarea>
             </div>
             <div class="mb-3 text-light">
                 <label class="form-label">Portada</label>
-                <input type="file" class="form-control">
+                <input accept="image/png, image/jpeg" type="file" class="form-control" v-on:change="onImageChange">
             </div>
             <div class="mb-3 text-light">
                 <label class="form-label">PDF</label>
-                <input type="file" class="form-control">
+                <input accept="application/pdf" type="file" class="form-control" v-on:change="onFileChange">
             </div>
-            
-        </form>
-        <div class="d-grid gap-2">
-                <button class="btn text-light" style="background-color: #2B5CBA;">Guardar</button>
+            <div class="d-grid gap-2">
+                <button type="submit" class="btn text-light" style="background-color: #2B5CBA;">Guardar</button>
                 <button class="btn btn-danger text-light" data-bs-dismiss="modal">Cerrar</button>
-            </div>
+        </div>
+        </form>
       </div>
     </div>
   </div>
@@ -84,27 +115,100 @@
 </template>
 
 <script>
+import axios from 'axios';
+import url from '../../../enviroment'
 export default {
+    mounted(){
+        this.getBooks();
+        this.getCategories()
+    },
     data(){
         return{
-            books:[
-                {id:1,title:"Libro 1", category:"Terror", author:"Alguien", portada:"Portada", pdf:"PDF"},
-                {id:2,title:"Libro 2", category:"Terror", author:"Alguien", portada:"Portada", pdf:"PDF"},
-                {id:3,title:"Libro 3", category:"Terror", author:"Alguien", portada:"Portada", pdf:"PDF"},
-                {id:4,title:"Libro 4", category:"Terror", author:"Alguien", portada:"Portada", pdf:"PDF"},
-                {id:5,title:"Libro 5", category:"Terror", author:"Alguien", portada:"Portada", pdf:"PDF"},            
-            ],
-            book:{},
-            categories:[
-                {category:"Terror"},
-                {category:"Romance"},
-                {category:"Fantasia"}
-            ],
-            category:""
+            books:[],
+            book:{
+                id:0,
+                titulo:"",
+                autor:"",
+                editorial:"",
+                isbn:"",
+                numero_paginas:null,
+                edicion:"",
+                año_lanzamiento:"",
+                sinopsis:"",
+                pdf:null,
+                imagen:null,
+                category_id:0
+            },
+            pdf:'',
+            imagen:'',
+            categories:[],
+            category:"",
+            loading:false,
+            token:'1|Q53XnsygM0czyZeAhJJXdN9RVVcghXGZSrNYSQYz'
         }
     },
     methods:{
-        
+        async getBooks(){
+            this.loading = true;
+            await axios.get(url+'book',{
+                headers:{
+                    Authorization: "Bearer " + this.token,
+                }
+            }).then(r=>{
+                this.loading = false;
+                this.books = r.data.books
+            })
+            .catch(err=>{
+
+            })
+        },
+        async getCategories(){
+            await axios.get(url+'categories',{
+                headers:{
+                    Authorization: "Bearer " + this.token,
+                }
+            }).then(r=>{
+                this.loading = false;
+                this.categories = r.data.categories
+            })
+            .catch(err=>{
+
+            })
+        },
+        onFileChange(e){
+            this.pdf = e.target.files[0]
+        },
+        onImageChange(e){
+            this.imagen = e.target.files[0]
+        },
+        save(e){
+            e.preventDefault();
+            let form = new FormData()
+
+            form.append('titulo',this.book.titulo)
+            form.append('autor',this.book.autor)
+            form.append('category_id',this.book.category_id)
+            form.append('editorial',this.book.editorial)
+            form.append('edicion',this.book.edicion)
+            form.append('numero_paginas',this.book.numero_paginas)
+            form.append('sinopsis',this.book.sinopsis)
+            form.append('isbn',this.book.isbn)
+            form.append('año_lanzamiento',this.book.año_lanzamiento)
+            form.append('pdf',this.pdf,this.pdf.name)
+            form.append('imagen',this.imagen,this.imagen.name)
+
+            axios.post(url+'book',form,{
+                headers:{
+                    Authorization: "Bearer " + this.token,
+                }
+            })
+            .then(r=>{
+                this.getBooks()
+            })
+            .catch(e=>{
+                console.log(e)
+            })
+        }
     }
 }
 </script>
@@ -120,5 +224,8 @@ export default {
     border: none;
     outline: none;
     cursor: pointer;
+}
+.border{
+    border-color: #2B5CBA;
 }
 </style>
