b4ff378c541c299f08abcdfd9d1f367b150d3880
Judecx14 <cesarjaviersanchhdz@gmail.com>
2022-12-04 14:50:29 -0600

    [ADD] manifest added

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,27 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: false,
+      },
+      manifest: {
+        name: 'H-Books',
+        short_name: 'H-Books',
+        description: 'Plataforma de libros electrónicos',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: './public/img/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './public/img/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        background_color: '#000000',
+        display: 'standalone',
+        start_url: '.', 
       }
     })
   ],
