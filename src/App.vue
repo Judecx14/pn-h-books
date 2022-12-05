f2922bfd1bff7c401754c745291f8190cbfaef6d
Judecx14 <cesarjaviersanchhdz@gmail.com>
2022-12-05 15:06:39 -0600

    [FIX] push notifications test

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,14 @@
 import NavVue from './components/Nav/Nav.vue'
 import FooterVue from './components/Footer/Footer.vue';
 import ViewRoutesVue from './components/ViewRoutes/ViewRoutes.vue';
+
 import axios from 'axios'
 export default {
   components:{ViewRoutesVue,NavVue, FooterVue},
   mounted(){},
   data(){},
-  methods:{}
+  methods:{},
+
 }
 </script>
 <style>
