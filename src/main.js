4564446a330e42a22d4ab6f1f1cbf70bffa602e9
Judecx14 <cesarjaviersanchhdz@gmail.com>
2022-12-05 13:59:30 -0600

    [UPDATE] one signal added

--- src/main.js
+++ src/main.js
@@ -4,5 +4,15 @@ import router from './router/index'
 import 'bootstrap/dist/css/bootstrap.css'
 import './assets/main.css'
 import 'bootstrap'
+import OneSignalVuePlugin from '@onesignal/onesignal-vue3'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .use(OneSignalVuePlugin, {
+        appId: "810465bf-8ebd-43c6-9b0a-7726265dde4d",
+        safari_web_id: "web.onesignal.auto.09714a24-a3bb-414f-8109-d75a4f07e6fa",
+        notifyButton: {
+            enable: true,
+        },
+    })
+    .mount('#app')
